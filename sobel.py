b68d55882cd4ea7ad0b7b6c7d17b8fe0b0c15d02
Kuntal Majumder <hellozee@disroot.org>
2019-02-08 00:56:04 +0530

    converted to numpy matrix

--- sobel.py
+++ sobel.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import numpy
 
 def gamma_correction(c_linear):
     c_srgb = 12.92 * c_linear 
@@ -8,18 +9,36 @@ def gamma_correction(c_linear):
 
 def convert_to_grayscale(image):
     width, height = image.size
-    pixels = im.load()
-    for x in range(width):
-        for y in range(height):
+    pixels = image.load()
+    matrix = numpy.empty((height,width),numpy.float64)
+    for x in range(height):
+        for y in range(width):
             r,g,b = pixels[x,y]
             c_linear = 0.2126 * (r/255) + 0.7152 * (g/255) + 0.0722 * (b/255)
-            c_srgb = gamma_correction(c_linear)
-            val = int(c_srgb * 255)
-            pixels[x,y] = (val,val,val)
+            matrix[x][y] = gamma_correction(c_linear)
+    return matrix
 
+def gaussian_blur(matrix):
+    pass
 
-im = Image.open("samples/blocks_color.jpg")
-original_image = Image.open("samples/blocks_color.jpg")
-convert_to_grayscale(im)
-im.show()
-original_image.show()
+def create_image(matrix,pixels):
+    height, width = matrix.shape
+    calc = lambda i, j : int(matrix[i][j] * 255)
+    make_tuple = lambda a : (a,a,a)
+    for x in range(height):
+        for y in range(width):
+            pixels[x,y] = make_tuple(calc(x,y))
+
+def main():
+    im = Image.open("samples/blocks_color.jpg")
+    original_image = Image.open("samples/blocks_color.jpg")
+    pixel_matrix = convert_to_grayscale(im)
+    gaussian_blur(pixel_matrix)
+    create_image(pixel_matrix,im.load())
+    im.show()
+    original_image.show()
+    im.close()
+    original_image.close()
+
+if __name__ == '__main__':
+    main()
