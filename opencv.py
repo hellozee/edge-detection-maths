2f96a432819de118037b3c72b8c149c354ddf99f
Kuntal Majumder <hellozee@disroot.org>
2019-02-09 17:06:01 +0530

    Applied the opencv's canny detector on the sample

--- opencv.py
+++ opencv.py
@@ -1,13 +1,7 @@
+#/usr/bin/python3
+
 import cv2
-import numpy as np
-from matplotlib import pyplot as plt
 
-img = cv2.imread('samples/fruit_color.jpg',0)
-edges = cv2.Canny(img,200,200)
-
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-
-plt.show()
+img = cv2.imread('samples/blocks_color.jpg',0)
+edges = cv2.Canny(img,100,200)
+cv2.imwrite("result/opencv.jpg", edges)
