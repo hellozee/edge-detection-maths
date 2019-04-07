e38b7a841be3ae4709e428482f627695fb539ce8
Kuntal Majumder <hellozee@disroot.org>
2019-04-07 21:40:57 +0530

    Added the heuristic function

--- graph.hh
+++ graph.hh
@@ -1,4 +1,19 @@
 #ifndef GRAPH_HH
 #define GRAPH_HH
 
+#include <cmath>
+
+struct point{
+    int x,y;
+};
+
+double heuristic_function(const point &p0, const point &pi, const point &pm){
+    double num = std::abs((pm.y-p0.y)*pi.x - (pm.x-p0.x)*pi.y + pm.x*p0.y + p0.x * pm.y);
+    double denom = std::sqrt(std::pow(pm.y - p0.y,2.0)+std::pow(pm.x-p0.x,2.0)); 
+    double di = num/denom;
+
+    double dm = std::sqrt(std::pow(pm.y-pi.y,2.0) + std::pow(pm.x-pi.x,2.0));
+    return 0.5*di + 0.5*dm;
+}
+
 #endif
